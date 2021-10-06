004ba1ecf3a5f950ed9c04ab9af9ed87f4647411
Aayush <ayxsth@gmail.com>
2021-10-06 20:02:17 +0545

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+const publicDirPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirPath));
+
+io.on("connection", (socket) => {
+    console.log("New WebSocket connection.");
+});
+
+server.listen(port, () => {
+    console.log(`The server is up at port ${port}.`);
+});
