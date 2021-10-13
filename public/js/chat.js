ec94437e1cb9d8535a4aa68b8a3a7b712639f8cc
Aayush <ayxsth@gmail.com>
2021-10-13 19:26:56 +0545

    displayed messages and location in the page with time

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,35 @@
 const socket = io();
 
+//Elements
 //naming convention for dom
 const $messageForm = document.querySelector("#message-form");
 const $messageFormInput = $messageForm.querySelector("input");
 const $messageFormButton = $messageForm.querySelector("button");
 const $locationButton = document.getElementById("location");
+const $messages = document.getElementById("messages");
 
-socket.on("message", (msg) => {
-    console.log(msg);
+//Templates
+const messageTemplate = document.getElementById("message-template").innerHTML;
+const locationMessageTemplate = document.getElementById(
+    "location-message-template"
+).innerHTML;
+
+socket.on("message", (message) => {
+    console.log(message);
+    const html = Mustache.render(messageTemplate, {
+        message: message.text,
+        createdAt: moment(message.createdAt).format("hh:mm a")
+    });
+    $messages.insertAdjacentHTML("beforeend", html);
+});
+
+socket.on("locationMessage", (message) => {
+    console.log(url);
+    const html = Mustache.render(locationMessageTemplate, {
+        url: message.url,
+        createdAt: moment(message.createdAt).format("hh:mm a")
+    });
+    $messages.insertAdjacentHTML("beforeend", html);
 });
 
 $messageForm.addEventListener("submit", (e) => {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 const Filter = require("bad-words");
+const {
+    generateMessage,
+    generateLocationMessage
+} = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
@@ -15,28 +19,30 @@ app.use(express.static(publicDirPath));
 
 io.on("connection", (socket) => {
     console.log("New WebSocket connection.");
-    socket.emit("message", "Welcome!");
-    socket.broadcast.emit("message", "A new user has joined!");
+    socket.emit("message", generateMessage("Welcome!"));
+    socket.broadcast.emit("message", generateMessage("A new user has joined!"));
 
     socket.on("sendMessage", (message, callback) => {
         if (new Filter().isProfane(message)) {
             return callback("Profanity is not allowed");
         }
 
-        io.emit("message", message);
+        io.emit("message", generateMessage(message));
         callback();
     });
 
     socket.on("sendLocation", (coords, callback) => {
         io.emit(
-            "message",
-            `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+            "locationMessage",
+            generateLocationMessage(
+                `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+            )
         );
         callback();
     });
 
     socket.on("disconnect", () => {
-        io.emit("message", "A user has left!");
+        io.emit("message", generateMessage("A user has left!"));
     });
 });
 
