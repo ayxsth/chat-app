5c11afb49e3afe1ba5d8c5affa347e7c75243b95
Aayush <ayxsth@gmail.com>
2021-10-19 10:51:56 +0545

    fixed room leave issue

--- src/utils/users.js
+++ src/utils/users.js
@@ -28,7 +28,7 @@ const addUser = ({ id, username, room }) => {
 const removeUser = (id) => {
     const index = users.findIndex((user) => user.id === id);
 
-    if (index !== 1) {
+    if (index !== -1) {
         return users.splice(index, 1)[0];
     }
 };
